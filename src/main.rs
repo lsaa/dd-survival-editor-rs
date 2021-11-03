7a68f58c5ed67b7b17b07599679fcbc2f0325cef
lsaa <lsaadev.personal@gmail.com>
2021-11-03 05:58:19 -0300

    better git

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,7 @@
-#![forbid(unsafe_code)]
-#![cfg_attr(not(debug_assertions), deny(warnings))] // Forbid warnings in release builds
-#![warn(clippy::all, rust_2018_idioms)]
 
-// When compiling natively:
 #[cfg(not(target_arch = "wasm32"))]
 fn main() {
-    let app = eframe_template::TemplateApp::default();
+    let app = dd_survival_editor_rs::TemplateApp::default();
     let native_options = eframe::NativeOptions::default();
     eframe::run_native(Box::new(app), native_options);
 }
